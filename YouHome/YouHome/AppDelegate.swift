707548d7400341dd354db2d8cf3d1db49a2882ff
dxiong7 <danielxiong523@gmail.com>
2019-05-02 19:23:06 -0700

    Implement login and signup with Parse

--- YouHome/YouHome/AppDelegate.swift
+++ YouHome/YouHome/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "YouHome"
+                configuration.server = "https://enigmatic-basin-50783.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
---
+++ YouHome/YouHome/LoginViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LoginViewController.swift
+//  YouHome
+//
+//  Created by Daniel Xiong on 5/2/19.
+//  Copyright © 2019 danielxiong523. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onLogin(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                print("error logging in")
+            }
+        }
+        
+    }
+    @IBAction func onSignup(_ sender: Any) {
+        
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        //user.email = "email@example.com"
+        // other fields can be set just like with PFObject
+        //user["phone"] = "415-392-0202"
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                print("error signing up")
+            }
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
